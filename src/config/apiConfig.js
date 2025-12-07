cf8d6ae47a93fbaf565c09804801ab0d6b0a51f7
r.yunusov <r.yunusov@podeli.ru>
2025-12-07 09:56:21 +0300

    change VITE_API_BASE_URL

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -5,4 +5,4 @@ export const API_MODE = import.meta.env.VITE_API_MODE || "real";
 
 // базовый URL реального бэка (на будущее)
 export const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL || "http://176.108.246.242:8090/api";
+    import.meta.env.VITE_API_BASE_URL || "http://176.108.246.242/api";
