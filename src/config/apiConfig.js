2aacdf1ff0378d528ed7a0c79e88d8506d08efd6
r.yunusov <r.yunusov@podeli.ru>
2026-01-16 00:33:29 +0300

    вернул страничку покупки совместимости.

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -5,5 +5,5 @@ export const API_MODE = import.meta.env.VITE_API_MODE || "real";
 
 // базовый URL реального бэка (на будущее)
 export const API_BASE_URL =
-    // import.meta.env.VITE_API_BASE_URL || "https://api.inner-code.ru/api";
-    import.meta.env.VITE_API_BASE_URL || "http://local-host:8090/api";
+    import.meta.env.VITE_API_BASE_URL || "https://api.inner-code.ru/api";
+    // import.meta.env.VITE_API_BASE_URL || "http://local-host:8090/api";
